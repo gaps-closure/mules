e0bf477ca528e0a31722dc0a258c88d059913015
Rajesh Krishnan <rkrishnan@perspectalabs.com>
2019-10-05 12:31:09 -0400

    example pragma clang attribute

---
+++ scratch/pragma-clang-example.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+char *x ="Hello World, Mars!\n";
+
+typedef struct precise_coords {
+  double lat;
+  double lon;
+  double alt;
+} *PreciseLocation;
+
+int main()
+{
+  #pragma clang attribute push (__attribute__((annotate("HIGH1"))), apply_to = any(function,type_alias,record,enum,variable,field))
+  PreciseLocation loc1;
+  PreciseLocation loc2;
+  #pragma clang attribute pop
+
+  #pragma clang attribute push (__attribute__((annotate("HIGH2"))), apply_to = any(function,type_alias,record,enum,variable,field))
+  char * secretKey;
+  #pragma clang attribute pop
+
+  char * foo = secretKey;
+  printf("%s",x);
+  return 0;
+}
+
