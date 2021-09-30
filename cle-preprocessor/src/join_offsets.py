c97388116d630163a2631a9833a02604e00863bc
Benjamin Flin <benjamin.flin@peratonlabs.com>
2021-09-30 19:02:14 +0000

    updated join_offsets

--- cle-preprocessor/src/join_offsets.py
+++ cle-preprocessor/src/join_offsets.py
@@ -2,15 +2,15 @@ import sys
 import pickle
 
 def main(oname, inlist):
-    all = {}#obj by 'cle-label'
-    for inf in inlist:
-        with open(inf ".offset.pkl","rb") as f:
-            in_one = pickle.load(f)
-            for dstline in in_one:
-                all[dstline] = in_one[dstline]
-    #print(json.dumps(list(all.values()), indent=2))
+    all = {}
+    def to_dict(fn):
+        with open(fn, 'rb') as f:
+            return pickle.load(f)
+    dicts = [ to_dict(f) for f in inlist ]
+    for d in dicts:
+        all.update(d)
     with open(oname, "wb") as of:
-        pickle.dump(all,of)
+        pickle.dump(all, of)
             
 if __name__ == "__main__":
     if len(sys.argv) < 3:
